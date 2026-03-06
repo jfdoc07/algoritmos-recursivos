171f1a15ecb3ebb82c58317f4e2f05f322c6c53d
jfdoc7 <jfcardenasd@udistrital.edu.co>
2026-03-06 09:44:18 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Home
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main obj = new Main();
+        int factorial = obj.calcularFactorial(6);
+        System.out.println(factorial);
+        System.out.println(obj.esPar(5));
+        System.out.println(obj.esImpar(3));
+    }
+
+    /*Función de recursión directa*/
+    public int calcularFactorial(int numero) {
+        if (numero == 0 || numero == 1) {                       //Caso base
+            return 1;
+        }
+        return numero * calcularFactorial(numero - 1);          //Caso recursivo
+    }
+
+    /*Funciones de recursión indirecta*/
+    public boolean esPar(int numero) {
+        if (numero == 0) {                                      //Caso base
+            return true;
+        }
+        return esImpar(numero - 1);                             //Caso recursivo, llama a la otra función.
+    }
+
+    public boolean esImpar(int numero) {
+        if (numero == 0) {                                      //Caso base
+            return false;
+        }
+        return esPar(numero - 1);                               //Caso recursivo, llama a la otra función.
+    }
+
+}
